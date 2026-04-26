6e2578542b171a35f827c69d8dee42dea0772fda
Askvernitel <danielkolotashvilio@gmail.com>
2026-04-26 21:22:12 +0400

    problem(2202b): TLE

---
+++ 2202b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void solve(){
+        int n;
+        cin >> n;
+        char a[n];
+        for(int i =0;i<n;i++){
+                cin >> a[i];
+        }
+
+        int l = 0, r = n-1;
+        deque<char> ans;
+        
+        
+        while(l < r){
+        }
+}
+int main(){ 
+        int tc;
+        cin >> tc;
+
+        while(tc--) solve();
+}
